d8e24a2714c0050b01113639e588cbd194d4eb6f
Shahid Ameen <iamshahidameen@gmail.com>
2022-08-10 21:51:08 +0500

    setup' next and prev buttons functionality

--- src/App.js
+++ src/App.js
@@ -3,18 +3,33 @@ import { useFetch } from './useFetch';
 import Follower from './Follower';
 function App() {
   const { loading, data } = useFetch();
-  const [page, setPage] = useState(5);
+  const [page, setPage] = useState(0);
   const [followers, setFollowers] = useState([]);
-
-  useEffect(() => {
-    if (loading) return;
-    setFollowers(data[page]);
-  }, [loading, page]);
   const handlePage = (e) => {
     setPage(e.target.dataset.handle);
     console.log(e.target.dataset.handle);
     console.log(page);
   };
+  const handlePagination = (pageValue) => {
+    if (pageValue === 'next') {
+      if (page >= data.length - 1) {
+        setPage(0);
+      } else {
+        setPage(page + 1);
+      }
+    }
+    if (pageValue === 'prev') {
+      if (page <= 0) {
+        setPage(data.length - 1);
+      } else {
+        setPage(page - 1);
+      }
+    }
+  };
+  useEffect(() => {
+    if (loading) return;
+    setFollowers(data[page]);
+  }, [loading, page]);
   return (
     <main>
       <div className="section-title">
@@ -28,25 +43,28 @@ function App() {
           })}
         </div>
         <div className="btn-container">
-          <button className="prev-btn">prev</button>
+          <button className="prev-btn" onClick={() => handlePagination('prev')}>
+            prev
+          </button>
 
-          {data.map((pageinate, index) => {
+          {data.map((paginate, index) => {
             return (
-              <>
-                <button
-                  className={
-                    index === Number(page) ? 'page-btn active-btn' : 'page-btn'
-                  }
-                  data-handle={index}
-                  onClick={handlePage}
-                >
-                  {index + 1}
-                </button>
-              </>
+              <button
+                key={index}
+                className={
+                  index === Number(page) ? 'page-btn active-btn' : 'page-btn'
+                }
+                data-handle={index}
+                onClick={handlePage}
+              >
+                {index + 1}
+              </button>
             );
           })}
 
-          <button className="next-btn">next</button>
+          <button className="next-btn" onClick={() => handlePagination('next')}>
+            next
+          </button>
         </div>
       </section>
     </main>
--- src/useFetch.js
+++ src/useFetch.js
@@ -9,10 +9,7 @@ export const useFetch = () => {
   const getProducts = async () => {
     const response = await fetch(url);
     const data = await response.json();
-    // paginate(data);
-    // console.log(paginate(data), 'this is usefetch reurn of paginate');
     setData(paginate(data));
-    console.log(paginate(data), 'this is paginate');
     setLoading(false);
   };
 
